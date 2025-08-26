81e272dbcd7fea06f4e62d7ea8939e65b04adce3
danielpbittner <fakeemail@gmail.com>
2025-08-25 19:37:56 -0500

    3rd Commit

---
+++ ClassList.py
@@ -0,0 +1,15 @@
+# Make a list that asks classmates for their names and stores them in a list.
+# Then print the list.
+# Daniel Bittner Aug 25, 2025
+
+
+names = []      # Makes a list to store names
+while True:     # Initiate loop to keep asking for names
+    name = input("Enter a classmate's name (or type 'quit' to stop): ")
+    if name.lower() == 'quit':
+        break
+    names.append(name)
+
+print("These are your classmates: ")
+for all_names in names:         # all_names is a temp variable
+    print(all_names)
